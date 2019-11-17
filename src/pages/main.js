f4985d288cbc175496c13537d5cd70591cbd0dae
LuisBarufi <luisbarufi@gmail.com>
2019-11-17 20:48:27 -0300

    updated

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
+import api from '../services/api';
+
 import { View, Text } from 'react-native';
 
-export default class Main extends Component {  
+export default class Main extends Component {
+  // método inicial disparado automáticamente 
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  
+  loadProducts = async () => {
+    const response = await api.get('/products')
+
+    const { docs } = response.data
+
+    console.log(docs);
+  };
+
   render() {
     return (
       <View>
