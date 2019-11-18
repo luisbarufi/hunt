4d76447ac4b3c7967b569d73377ff7c62e366c4c
LuisBarufi <luisbarufi@gmail.com>
2019-11-17 21:21:12 -0300

    updated

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
 import api from '../services/api';
 
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 
 export default class Main extends Component {
+  state = {
+    docs: [],
+  };
+
   // método inicial disparado automáticamente 
   componentDidMount() {
     this.loadProducts();
   }
-
   
   loadProducts = async () => {
     const response = await api.get('/products')
 
     const { docs } = response.data
 
-    console.log(docs);
+    this.setState({ docs });
   };
+
+  renderItem = ({ item }) => (
+    <View>
+      <Text>{item.title}</Text>
+      <Text>{item.description}</Text>
+
+      <TouchableOpacity onPress={() => {}}>
+        <Text>Acessar</Text>
+      </TouchableOpacity>
+    </View>
+  );
 
   render() {
     return (
       <View>
-        <Text>Página Main</Text>
+        <FlatList
+          data={this.state.docs}
+          keyExtractor={item => item._id}
+          renderItem={this.renderItem}
+        />
       </View>
     );
   }
